b5c0c1e2e732102c8a54a1bcbddef2b23c1df016
chrissetiana <chrissetiana@gmail.com>
2018-07-31 23:48:47 +0100

    Polished a bit

--- app/src/main/java/com/chrissetiana/tidereport/MainActivity.java
+++ app/src/main/java/com/chrissetiana/tidereport/MainActivity.java
@@ -3,27 +3,13 @@ package com.chrissetiana.tidereport;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+    private static final String REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-12-31&minmagnitude=5&limit=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
         alertView.setText(getAlert(tsunami.alert));
     }
 
+    private String getDate(long time) {
+        return new SimpleDateFormat("EEE, d MMM yyyy '\n'h:mm:ss a '\n'z").format(time);
+    }
+
+    private String getAlert(int tsunamiAlert) {
+        switch (tsunamiAlert) {
+            case 0:
+                return getString(R.string.alert_no);
+            case 1:
+                return getString(R.string.alert_yes);
+            default:
+                return getString(R.string.alert_not_available);
+        }
+    }
+
     private class TsunamiAsyncTask extends AsyncTask<String, Void, Tsunami> {
 
         protected Tsunami doInBackground(String... urls) {
@@ -62,21 +63,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
             setViews(result);
-        }
-    }
-
-    private String getDate(long time) {
-        return new SimpleDateFormat("EEE, d MMM yyyy 'at' HH:mm:ss z").format(time);
-    }
-
-    private String getAlert(int tsunamiAlert) {
-        switch (tsunamiAlert) {
-            case 0:
-                return getString(R.string.alert_no);
-            case 1:
-                return getString(R.string.alert_yes);
-            default:
-                return getString(R.string.alert_not_available);
         }
     }
 }
